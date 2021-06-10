34e9502ad2a2a6caf9385b6b5c54173b31ccc83c
Herbert Paes <herbert_pbarreto@hotmail.com>
2021-06-10 14:12:28 -0300

    Componente Summary

--- Chapter-02/dtmoney/src/styles/global.ts
+++ Chapter-02/dtmoney/src/styles/global.ts
@@ -8,6 +8,7 @@ const GlobalStyle = createGlobalStyle`
 
         --red: #E52E4D;
         --blue: #5429CC;
+        --green: #33CC95;
         --blue-light: #6933FF;
 
         --text-title:#363F5F;
