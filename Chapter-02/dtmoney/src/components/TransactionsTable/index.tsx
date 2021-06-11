455d8c335cf73d8d33e7b408fd2aa8499f4bd2e5
Herbert Paes <herbert_pbarreto@hotmail.com>
2021-06-11 13:56:48 -0300

    Configurando MirageJS

--- Chapter-02/dtmoney/src/components/TransactionsTable/index.tsx
+++ Chapter-02/dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,33 +1,40 @@
-import {Container} from './styles';
-export function TransactionsTable() {
-    return (
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Titulo</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
+import React, { useEffect } from "react";
+import { Container } from "./styles";
 
-                <tbody>
-                    <tr>
-                        <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>10/06/20211</td>
-                    </tr>
-                    <tr>
-                        <td>Aluguel</td>
-                        <td className="withdraw">R$1.100</td>
-                        <td>Casa</td>
-                        <td>10/06/20211</td>
-                    </tr>
-                    
-                </tbody>
-            </table>
-        </Container>
-    )
+export function TransactionsTable() {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transactions")
+    .then((response) =>  response.json())
+    .then(data => console.log(data));
+  }, []);
+
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>10/06/20211</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">R$1.100</td>
+            <td>Casa</td>
+            <td>10/06/20211</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 }
