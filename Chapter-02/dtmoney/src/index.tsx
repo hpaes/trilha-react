a5aff76e43325d8b82626c16601bf89d9289238f
Herbert Paes <herbert_pbarreto@hotmail.com>
2021-06-20 16:00:26 -0300

    Listando transações e seeds

--- Chapter-02/dtmoney/src/index.tsx
+++ Chapter-02/dtmoney/src/index.tsx
@@ -5,18 +5,42 @@ import { App } from "./App";
 
 createServer({
   models: {
-    transactions: Model,
+    transaction: Model,
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 110,
+          createdAt: new Date("2021-02-14 09:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return this.schema.all('transaction');
+      return this.schema.all("transaction");
     });
 
     this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
-      return schema.create("transactions", data);
+      return schema.create("transaction", data);
     });
   },
 });
